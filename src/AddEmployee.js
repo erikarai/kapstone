8adb216fb2b6ae73f3fc296bc961f03c974cd6bd
Ashwani Kumar Maurya <ashvanikumarmaurya8@gmail.com>
2023-09-27 10:55:39 +0530

    yes

--- src/AddEmployee.js
+++ src/AddEmployee.js
@@ -1,63 +1,95 @@
 import axios from 'axios';
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 function AddEmployee() {
-	const [data, setData] = useState({
-		Idea: '',
-		email: '',
-		password: '',
-		Description: '',
+  const [data, setData] = useState({
+    ideaName: '',
+    emails: '', // Change to store comma-separated email addresses
+    password: '',
+    description: '',
+  });
 
-	})
-	const navigate = useNavigate()
+  const navigate = useNavigate();
 
-	const handleSubmit = (event) => {
-		event.preventDefault();
-		const formdata = new FormData();
-		formdata.append("name", data.name);
-		formdata.append("email", data.email);
-		formdata.append("password", data.password);
-		formdata.append("address", data.address)
-		axios.post('http://localhost:8081/create', formdata)
-		.then(res => {
-			navigate('/employee')
-		})
-		.catch(err => console.log(err));
-	}
-	return (
-		<div className='d-flex flex-column align-items-center pt-4'>
-			<h2>Add New Ideas</h2>
-			<form class="row g-3 w-50" onSubmit={handleSubmit}>
-			<div class="col-12">
-					<label for="inputName" class="form-label">Idea Name</label>
-					<input type="text" class="form-control" id="inputName" placeholder='Enter Name' autoComplete='off'
-					onChange={e => setData({...data, name: e.target.value})}/>
-				</div>
-				<div class="col-12">
-					<label for="inputEmail4" class="form-label">Email</label>
-					
-					<input type="email" class="form-control" id="inputEmail4" placeholder='abc@gmail.com' autoComplete='off'
-					onChange={e => setData({...data, email: e.target.value})}/>
-				</div>
-				<div class="col-12">
-					<label for="inputPassword4" class="form-label">Password</label>
-					<input type="password" class="form-control" id="inputPassword4" placeholder='Enter Password'
-					 onChange={e => setData({...data, password: e.target.value})}/>
-				</div>
-				
-				<div class="col-12">
-					<label for="inputAddress" class="form-label" rows="8">Description</label>
-					<input type="text" class="form-control" id="inputAddress"  autoComplete='off' rows="8"
-					onChange={e => setData({...data, address: e.target.value})}/>
-				</div>
-				<div class="col-12">
-					<button type="submit" class="btn btn-primary">Submit</button>
-				</div>
-			</form>
-		</div>
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formdata = new FormData();
+    formdata.append('ideaName', data.ideaName);
+    formdata.append('emails', data.emails); // Update to store emails
+    formdata.append('password', data.password);
+    formdata.append('description', data.description);
 
-	)
+    axios
+      .post('http://localhost:8081/create', formdata)
+      .then((res) => {
+        navigate('/employee');
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <div className='d-flex flex-column align-items-center pt-4'>
+      <h2>Add New Ideas</h2>
+      <form className='row g-3 w-50' onSubmit={handleSubmit}>
+        <div className='col-12'>
+          <label htmlFor='inputIdeaName' className='form-label'>
+            Idea Name
+          </label>
+          <input
+            type='text'
+            className='form-control'
+            id='inputIdeaName'
+            placeholder='Enter Name'
+            autoComplete='off'
+            onChange={(e) => setData({ ...data, ideaName: e.target.value })}
+          />
+        </div>
+        <div className='col-12'>
+          <label htmlFor='inputEmails' className='form-label'>
+            Email Addresses (comma-separated)
+          </label>
+          <input
+            type='text'
+            className='form-control'
+            id='inputEmails'
+            placeholder='abc@gmail.com, def@example.com'
+            autoComplete='off'
+            onChange={(e) => setData({ ...data, emails: e.target.value })}
+          />
+        </div>
+        <div className='col-12'>
+          <label htmlFor='inputPassword' className='form-label'>
+            Password
+          </label>
+          <input
+            type='password'
+            className='form-control'
+            id='inputPassword'
+            placeholder='Enter Password'
+            onChange={(e) => setData({ ...data, password: e.target.value })}
+          />
+        </div>
+        <div className='col-12'>
+          <label htmlFor='inputDescription' className='form-label'>
+            Description
+          </label>
+          <input
+            type='text'
+            className='form-control'
+            id='inputDescription'
+            autoComplete='off'
+            onChange={(e) => setData({ ...data, description: e.target.value })}
+          />
+        </div>
+        <div className='col-12'>
+          <button type='submit' className='btn btn-primary'>
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
-export default AddEmployee
+export default AddEmployee;
